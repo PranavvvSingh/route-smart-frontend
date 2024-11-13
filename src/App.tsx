f2bc0eff5b2dcfc64454aab33fd0aadd8b3fda01
PranavvvSingh <pranavvalt@gmail.com>
2024-11-13 17:26:21 +0530

    add: header and home

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,13 @@
-import { useEffect, useRef, useState } from "react"
-import mapboxgl, { Marker } from "mapbox-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
-import { Geocoder } from "@mapbox/search-js-react"
+import Header from "./components/header"
+import Home from "./components/home"
 
-const TOKEN = import.meta.env.VITE_MAPBOX
-
-function App() {
-   const mapRef = useRef<mapboxgl.Map | null>(null)
-   const mapContainerRef = useRef<HTMLDivElement>(null)
-   const [inputValue, setInputValue] = useState("")
-   const [isMapLoaded, setIsMapLoaded] = useState(false)
-   const [markers, setMarkers] = useState<Marker[]>([])
-
-   const handleRetrieve = (result: any) => {
-      console.log("retrieve")
-      if (result) {
-         const coordinates = result.geometry.coordinates
-         if (mapRef.current) {
-            const newMarker = new mapboxgl.Marker({
-               color: markers.length === 0 ? "red" : "green",
-            })
-               .setLngLat(coordinates)
-               .setPopup(
-                  new mapboxgl.Popup({
-                     closeOnMove: true,
-                  }).setText(
-                     `Latitude: ${coordinates[0]} Longitude: ${coordinates[1]}`,
-                  ),
-               )
-               .addTo(mapRef.current)
-            // console.log(newMarker)
-            setMarkers((prevMarkers) => [...prevMarkers, newMarker])
-            setInputValue("")
-         }
-      }
-   }
-
-   const removeMarker = (index: number) => {
-      markers[index].remove()
-      setMarkers((prevMarkers) => prevMarkers.filter((_, i) => i !== index))
-   }
-
-   useEffect(() => {
-      if (!mapContainerRef.current) return
-
-      mapboxgl.accessToken = TOKEN
-      const map = new mapboxgl.Map({
-         container: mapContainerRef.current,
-         center: [77.2088, 28.6139],
-         style: "mapbox://styles/mapbox/dark-v11",
-         zoom: 5,
-      })
-      mapRef.current = map
-
-      mapRef.current.on("load", () => {
-         setIsMapLoaded(true)
-         console.log("loaded")
-      })
-
-      return () => {
-         if (mapRef.current) mapRef.current.remove()
-      }
-   }, [])
-
+const App = () => {
    return (
-      <div className="h-[100vh] w-[100vw] flex p-5 items-center">
-         <div className="w-[40%] h-[400px]">
-            {isMapLoaded && mapRef.current && (
-               <Geocoder
-                  accessToken={TOKEN}
-                  map={mapRef.current}
-                  mapboxgl={mapboxgl}
-                  value={inputValue}
-                  onChange={(d) => {
-                     setInputValue(d)
-                  }}
-                  onRetrieve={handleRetrieve}
-                  marker={{
-                     color: "green",
-                  }}
-                  theme={{
-                     variables: {
-                        fontFamily: "helvetica neue, sans-serif",
-                        border: "2px solid #424242",
-                        borderRadius: "5px",
-                        colorText: "#EDEADE",
-                        colorBackdrop: "#EDEADE",
-                        // colorPrimary: "#08216b",
-                        colorBackground: "#212121",
-                        colorBackgroundHover: "#474747",
-                        fontWeight: "400",
-                        fontWeightBold: "400",
-                        fontWeightSemibold: "400",
-                        lineHeight: "1.4",
-                     },
-                     cssText: `
-                                 [class$="--Input"] {
-                                 color: #EDEADE;
-                                 border: none;
-                                 }
-                                 [class$="--Input"]:focus {
-                                 color: #EDEADE;
-                                 border: none;
-                                 }
-                                 [class$="--Input"]::placeholder {
-                                 color: #EDEADE;
-                                 opacity: 0.7;
-                                 }
-                              `,
-                  }}
-               />
-            )}
-            {markers.map((_item, index) => (
-               <div onClick={() => removeMarker(index)}>{index}</div>
-            ))}
+      <div className="h-[100vh] w-[100vw] flex flex-col m-0">
+         <Header />
+         <div className="flex-grow bg-[#0B0B0B]">
+            <Home />
          </div>
-         <div className="flex-grow h-[100%]" ref={mapContainerRef} />
       </div>
    )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+   return (
+      <div className="h-[70px] flex justify-between items-center text-white bg-[#100C08] p-5">
+         <h1>Route Smart</h1>
+         <nav className="flex gap-5 items-center justify-center">
+            <p>Home</p>
+            <p>Maps</p>
+            <p>Sign In</p>
+         </nav>
+      </div>
+   )
+}
+
+export default Header
